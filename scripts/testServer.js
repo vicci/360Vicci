6b304d6cb554b64550c930e9fea4b64c22030aaa
Joey Cozza <joeycozza@gmail.com>
2013-12-07 16:06:58 -0700

    trying to get mysql working with node, just a test

---
+++ scripts/testServer.js
@@ -0,0 +1,54 @@
+
+var express = require('express');
+var mysql = require('mysql');
+var http = require('http');
+var path = require('path');
+
+va
+
+r connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root@localhost',
+  port: '8888',
+  database: 'api_node'
+});
+
+connection.connect(function(err) {
+	console.log(err);
+});
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
+
+
+
+
+var app = express()
+var clientDir = path.join(__dirname, '../app')
+
+app.configure(function() {
+  app.set('port', 1337)
+  app.use(app.router)
+  app.use(express.static(clientDir))
+})
+
+app.get('/', function(req, res) {
+  res.sendfile(path.join(clientDir, 'index.html'))
+})
+
+app.get('/events', function(req, res) {
+  res.sendfile(path.join(clientDir, 'events.html'))
+})
+
+
+var server = http.createServer(app)
+
+server.listen(app.get('port'), function() {
+  console.log('listening on: %d', app.get('port'))
+});
